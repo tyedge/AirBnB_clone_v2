e42027db761cf34978bf5a8d9ee60b61ad736171
tyedge <tyedge.inc@gmail.com>
2019-09-02 18:36:43 +0000

    Adding project files

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def try_this():
+    """ This function display 'Hello HBNB!' """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
