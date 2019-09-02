f30faeeea79f2752813292d666eb049030869017
tyedge <tyedge.inc@gmail.com>
2019-09-02 19:13:10 +0000

    Changed return value from HBNB! to HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,7 +13,7 @@ def try_this():
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ This function displays 'HBNB!' """
-    return "HBNB!"
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
